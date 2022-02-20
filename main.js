84325ef144d5862b955ce1097e9019e0345ac276
Neel <shahneel169@gmail.com>
2022-02-20 17:40:45 +0530

    Arrays

--- main.js
+++ main.js
@@ -1,33 +1,78 @@
-// String and String methods
+// //================== String and String methods
 
-const name = 'neel';
-const age = 22;
-
-
-// concatenation
-console.log('My name is ' + name + ' and I am ' + age);
-
-//Template string
-const hello = `My name is ${name} and I am ${age}`;
-console.log(hello);
+// const name = 'neel';
+// const age = 22;
 
 
-const s = 'Hello world!';
+// // concatenation
+// console.log('My name is ' + name + ' and I am ' + age);
 
-// property --> without parenthesis
-// method --> with parenthesis
+// //Template string
+// const hello = `My name is ${name} and I am ${age}`;
+// console.log(hello);
 
-// length
-console.log(s.length);
 
-// convert uppercase
-console.log(s.toUpperCase());
+// const s = 'Hello world!';
 
-// convert lowercase
-console.log(s.toLowerCase());
+// // property --> without parenthesis
+// // method --> with parenthesis
 
-// substring
-console.log(s.substring(0,5).toUpperCase());
+// // length
+// console.log(s.length);
 
-//split into array
-console.log(s.split(' '));
+// // convert uppercase
+// console.log(s.toUpperCase());
+
+// // convert lowercase
+// console.log(s.toLowerCase());
+
+// // substring
+// console.log(s.substring(0,5).toUpperCase());
+
+// //split into array
+// console.log(s.split(' '));
+
+// //================== String and String methods
+
+
+
+// ======================= Arrays - variables that holds multiple values
+
+
+// Create array using array constructor
+const numbers = new Array(1,2,3,4,5);
+
+console.log(numbers);
+
+
+const fruits = ['apples', 'oranges', 'pears', 10, true];
+
+console.log(fruits);
+
+// accessing items 
+console.log(fruits[1]);
+
+//adding item
+fruits[3] = 'graps';
+console.log(fruits);
+
+// push item at the end 
+fruits.push('mangos');
+console.log(fruits);
+
+// push item at the start
+fruits.unshift('strawberries');
+console.log(fruits);
+
+//to remove last item
+fruits.pop()
+console.log(fruits);
+
+//to check if it's array
+console.log(Array.isArray(fruits));
+console.log(Array.isArray('hello'));
+
+// get index of item 
+console.log(fruits.indexOf('oranges'));
+
+// ======================= Arrays - variables that holds multiple values
