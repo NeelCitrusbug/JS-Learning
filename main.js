4af9932edd180f6484faed421814946ca8fab6fc
Neel <shahneel169@gmail.com>
2022-02-20 17:19:24 +0530

    String & String methods

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log("hello world")
+// String and String methods
+
+const name = 'neel';
+const age = 22;
+
+
+// concatenation
+console.log('My name is ' + name + ' and I am ' + age);
+
+//Template string
+const hello = `My name is ${name} and I am ${age}`;
+console.log(hello);
+
+
+const s = 'Hello world!';
+
+// property --> without parenthesis
+// method --> with parenthesis
+
+// length
+console.log(s.length);
+
+// convert uppercase
+console.log(s.toUpperCase());
+
+// convert lowercase
+console.log(s.toLowerCase());
+
+// substring
+console.log(s.substring(0,5).toUpperCase());
+
+//split into array
+console.log(s.split(' '));
